560197f164e62e62b5f39eafaa49e44feff5aae5
Gayatri Dunakhe GD <74578232+GayatriDunakhe@users.noreply.github.com>
2022-07-19 18:53:54 +0530

    Added two img files

---
+++ Image Gallary using api/Services/Constants.dart
@@ -0,0 +1,1 @@
+const API_ACCESS_KEY = '9-idiEI7Ys3I-_jaaiImK-8ZIVNftNr3_6Zd_gN2gYE';
---
+++ Image Gallary using api/Services/String.dart
@@ -0,0 +1,6 @@
+
+import 'Constants.dart';
+
+var imgApi = "https://api.unsplash.com/photos/?client_id=${API_ACCESS_KEY}";
+
+String getImgApi() => imgApi;
