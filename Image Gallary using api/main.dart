3c88ad010b934b7bee9054858096b34afcc74b35
Gayatri Dunakhe GD <74578232+GayatriDunakhe@users.noreply.github.com>
2022-07-19 18:53:00 +0530

    added api

--- Image Gallary using api/main.dart
+++ Image Gallary using api/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'Services/API/ImageApi.dart';
 import 'Services/Functions.dart';
 import 'Services/Model/ImageModel.dart';
 import 'Services/Widgets/ImageContainer.dart';
@@ -33,8 +34,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   List imgList = [];
-  
-  bool isLoading = false;
+
+  bool isLoading = true;
+
+  bool isRetry = true;
 
   @override
   void initState() {
@@ -49,92 +52,129 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text("Abstract Images", style: cstmTextStyle(fc: Colors.black),),
-            isLoading
-            ? Center(
-              child: Container(
-                margin: EdgeInsets.all(10.0),
-                padding: EdgeInsets.all(200.0),
-                height: 500,
-                width: 500,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  gradient: LinearGradient(
-                    colors: [
-                      Colors.purple,
-                      Colors.pinkAccent.shade200,
-                    ],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
+        child: Center(
+          child: Column(
+            children: [
+              Text("Abstract Images", style: cstmTextStyle(fc: Colors.black),),
+              isLoading
+              ? Center(
+                child: Container(
+                  margin: EdgeInsets.all(10.0),
+                  padding: EdgeInsets.all(200.0),
+                  height: 500,
+                  width: 500,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.purple,
+                        Colors.pinkAccent.shade200,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
                   ),
+                  child: CircularProgressIndicator(),
                 ),
-                child: CircularProgressIndicator(),
+              )
+              : isRetry
+              ? Column(
+                children: [
+                  Text("Unable to load images", style: cstmTextStyle(fc: Colors.black),),
+                  IconButton(onPressed: (){
+                    getImgList();
+                  }, icon: const Icon(Icons.replay),),
+                ],
+              )
+              : GridView.builder(
+                padding: EdgeInsets.all(20.0),
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 20.0, crossAxisSpacing: 20.0),
+                itemBuilder: (BuildContext c, int index){
+                  return ImageContainer(
+                    imageModel: imgList[index],
+                  );
+                },
+                itemCount: imgList.length,
               ),
-            )
-            : GridView.builder(
-              padding: EdgeInsets.all(20.0),
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 20.0, crossAxisSpacing: 20.0),
-              itemBuilder: (BuildContext c, int index){
-                return ImageContainer(
-                  imageModel: imgList[index],
-                );
-              },
-              itemCount: imgList.length,
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
-  void getImgList() {
-    ImageModel imgModel = ImageModel(
-      imgId: "1",
-      imgUrl: "https://static.vecteezy.com/packs/media/vectors/term-bg-1-666de2d9.jpg",
-      imgLikes: "14",
-    );
+  Future<void> getImgList() async {
+    setState(() {
+      isRetry = false;
+      isLoading = true;
+    });
 
-    ImageModel imgModel2 = ImageModel(
-      imgId: "2",
-      imgUrl: "https://media.istockphoto.com/vectors/gradient-blob-abstract-background-vector-id1282505760?k=20&m=1282505760&s=612x612&w=0&h=REKlz8PanglihiT_Z4_MY3eJhAMRe4F8zfPp0rVzccg=",
-      imgLikes: "40",
-    );
+    List<ImageModel> tempImgModel = await fetchImageApi();
 
-    ImageModel imgModel3 = ImageModel(
-      imgId: "3",
-      imgUrl: "https://images.unsplash.com/photo-1633596683562-4a47eb4983c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHJlbmRlcnN8ZW58MHx8MHx8&w=1000&q=80",
-      imgLikes: "47",
-    );
+    if(tempImgModel.isEmpty){
+      print("Error in api calling");
+      setState(() {
+        isRetry = true;
+        isLoading = false;
+      });
+    }
+    else{
+      setState(() {
+        imgList.clear();
+        imgList.addAll(tempImgModel);
+        isLoading = false;
+        isRetry = false;
+//      print(imgList);
+      });
+    }
 
-    ImageModel imgModel4 = ImageModel(
-      imgId: "4",
-      imgUrl: "https://media.istockphoto.com/photos/multi-colored-fingerprint-picture-id1384541738?b=1&k=20&m=1384541738&s=170667a&w=0&h=Az8TOQClvPmQY_9OF0Pfr6GFcKa5BNoU-xWVCPhjZkg=",
-      imgLikes: "90",
-    );
+    
 
-    ImageModel imgModel5 = ImageModel(
-      imgId: "5",
-      imgUrl: "https://media.istockphoto.com/photos/blue-geometric-shape-abstract-technology-background-picture-id906577952?b=1&k=20&m=906577952&s=170667a&w=0&h=TwHPNpkDatPku2D6Ej3x6vcl-77wsc5E0fbqmXm7pho=",
-      imgLikes: "20",
-    );
-
-    ImageModel imgModel6 = ImageModel(
-      imgId: "6",
-      imgUrl: "https://static.vecteezy.com/system/resources/thumbnails/003/097/762/small/colorful-gradient-wavy-abstract-background-free-vector.jpg",
-      imgLikes: "56",
-    );
-
-    imgList.add(imgModel);
-    imgList.add(imgModel2);
-    imgList.add(imgModel3);
-    imgList.add(imgModel4);
-    imgList.add(imgModel5);
-    imgList.add(imgModel6);
-    //print(imgList);
+//    ImageModel imgModel = ImageModel(
+//      imgId: "1",
+//      imgUrl: "https://static.vecteezy.com/packs/media/vectors/term-bg-1-666de2d9.jpg",
+//      imgLikes: "14",
+//    );
+//
+//    ImageModel imgModel2 = ImageModel(
+//      imgId: "2",
+//      imgUrl: "https://media.istockphoto.com/vectors/gradient-blob-abstract-background-vector-id1282505760?k=20&m=1282505760&s=612x612&w=0&h=REKlz8PanglihiT_Z4_MY3eJhAMRe4F8zfPp0rVzccg=",
+//      imgLikes: "40",
+//    );
+//
+//    ImageModel imgModel3 = ImageModel(
+//      imgId: "3",
+//      imgUrl: "https://images.unsplash.com/photo-1633596683562-4a47eb4983c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHJlbmRlcnN8ZW58MHx8MHx8&w=1000&q=80",
+//      imgLikes: "47",
+//    );
+//
+//    ImageModel imgModel4 = ImageModel(
+//      imgId: "4",
+//      imgUrl: "https://media.istockphoto.com/photos/multi-colored-fingerprint-picture-id1384541738?b=1&k=20&m=1384541738&s=170667a&w=0&h=Az8TOQClvPmQY_9OF0Pfr6GFcKa5BNoU-xWVCPhjZkg=",
+//      imgLikes: "90",
+//    );
+//
+//    ImageModel imgModel5 = ImageModel(
+//      imgId: "5",
+//      imgUrl: "https://media.istockphoto.com/photos/blue-geometric-shape-abstract-technology-background-picture-id906577952?b=1&k=20&m=906577952&s=170667a&w=0&h=TwHPNpkDatPku2D6Ej3x6vcl-77wsc5E0fbqmXm7pho=",
+//      imgLikes: "20",
+//    );
+//
+//    ImageModel imgModel6 = ImageModel(
+//      imgId: "6",
+//      imgUrl: "https://static.vecteezy.com/system/resources/thumbnails/003/097/762/small/colorful-gradient-wavy-abstract-background-free-vector.jpg",
+//      imgLikes: "56",
+//    );
+//
+//    imgList.add(imgModel);
+//    imgList.add(imgModel2);
+//    imgList.add(imgModel3);
+//    imgList.add(imgModel4);
+//    imgList.add(imgModel5);
+//    imgList.add(imgModel6);
+//    //print(imgList);
 
   }
 }
