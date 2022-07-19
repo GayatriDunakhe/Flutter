c37c0afd9092f616635b41618586820efda20288
Gayatri Dunakhe GD <74578232+GayatriDunakhe@users.noreply.github.com>
2022-07-19 18:55:40 +0530

    update

--- Image Gallary using api/Services/String.dart
+++ Image Gallary using api/Services/String.dart
@@ -1,6 +1,6 @@
 
 import 'Constants.dart';
 
-var imgApi = "https://api.unsplash.com/photos/?client_id=${API_ACCESS_KEY}";
+var imgApi = "https://api.unsplash.com/photos/?client_id=${API_ACCESS_KEY}"; //Unsplash api
 
 String getImgApi() => imgApi;
