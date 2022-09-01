6f97662893dc0f435d0c6ab19f0f38d796dbef5b
Gayatri Dunakhe GD <74578232+GayatriDunakhe@users.noreply.github.com>
2022-09-01 22:01:55 +0530

    Update background

--- Fitness app/Components/Background.dart
+++ Fitness app/Components/Background.dart
@@ -4,6 +4,57 @@ import 'package:proste_bezier_curve/proste_bezier_curve.dart';
 BackgroundCircles(screenWidth){
 
   return Stack(
+    alignment: Alignment.bottomCenter,
+    children: [
+
+      Circles(),
+
+      ClipPath(
+        clipper: BackgroudCliper(),
+        child: Container(
+          height: 200,
+          color: Colors.white.withOpacity(0.30),
+        ),
+      ),
+
+      ClipPath(
+        clipper: BackgroudCliper(),
+        child: Container(
+          height: 180,
+          color: Colors.white,
+        ),
+      ),
+
+      Container(
+        height: 70,
+        width: screenWidth,
+        color: Colors.white,
+      ),
+
+    ],
+  );
+}
+
+
+class BackgroudCliper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0.0, 100);
+    path.quadraticBezierTo(size.width / 6, 60, size.width / 3, 100);
+    path.quadraticBezierTo(size.width / 2, 140, 2 * size.width / 3, 100);
+    path.quadraticBezierTo(5 * size.width / 6, 60, size.width, 100);
+    path.lineTo(0, size.width * 12);
+    path.lineTo(0, 0);
+    path.close();
+    return path;
+  }
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+}
+
+Circles(){
+  return Stack(
     alignment: Alignment.center,
     children: [
       Container(
@@ -32,28 +83,7 @@ BackgroundCircles(screenWidth){
           color: Colors.white,
         ),
       ),
-
-
-      ClipPath(
-        clipper: ProsteBezierCurve(
-            list: [
-              BezierCurveSection(
-                start: Offset(screenWidth, 0),
-                top: Offset(screenWidth / 4 * 3, 60),
-                end: Offset(screenWidth / 2, 30),
-              ),
-              BezierCurveSection(
-                start: Offset(screenWidth / 2, 30),
-                top: Offset(screenWidth / 4, 0),
-                end: Offset(0, 30),
-              ),
-            ]
-        ),
-        child: Container(
-          height: 200,
-          color: Colors.white.withOpacity(0.60),
-        ),
-      ),
     ],
   );
+
 }
