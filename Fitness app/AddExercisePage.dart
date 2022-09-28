a91fce5d071c082c95adf9ca7959138b781f6c81
Gayatri Dunakhe GD <74578232+GayatriDunakhe@users.noreply.github.com>
2022-09-28 23:08:05 +0530

    Added calender on file

--- Fitness app/AddExercisePage.dart
+++ Fitness app/AddExercisePage.dart
@@ -95,6 +95,23 @@ class _AddExercisePageState extends State<AddExercisePage> {
                       ],
                     ),
                   ),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: TableCalendar(
+                      calendarStyle: CalendarStyle(
+                        defaultTextStyle: cstmTextStyle(fs:18,fc: Colors.black54),
+                        weekendTextStyle: cstmTextStyle(fs:18,fc: Colors.black54),
+                        todayTextStyle: cstmTextStyle(fs:18,fc: Color(0xFFFF46B5).withOpacity(0.70),),
+                        todayDecoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,),
+                      ),
+                      headerVisible: false,
+                      focusedDay: DateTime.now(),
+                      firstDay: DateTime.utc(DateTime.monthsPerYear,),
+                      lastDay: DateTime.utc(2900, 8, 31),
+                    ),
+                  ),
                 ],
               ),
             ),
