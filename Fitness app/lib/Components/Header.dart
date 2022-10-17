951635d583d687479f8c57304e971fea14a7a48f
Gayatri Dunakhe GD <74578232+GayatriDunakhe@users.noreply.github.com>
2022-10-17 12:36:51 +0530

    Update file with new header -exercise header

--- Fitness app/lib/Components/Header.dart
+++ Fitness app/lib/Components/Header.dart
@@ -51,3 +51,32 @@ ExerciseHeader(){
   );
 }
 
+ExerciseHeader(context){
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      IconButton(
+        onPressed: (){
+          Navigator.pop(context);
+        },
+        icon: Icon(Icons.arrow_back),
+        splashRadius: 20,
+      ),
+      Expanded(
+          child: Text(
+            "Exercise",
+            textAlign: TextAlign.center,
+            style: cstmTextStyle(fs:24,fc: Colors.white),
+
+          )),
+      IconButton(
+        onPressed: (){},
+        icon: Icon(Icons.more_horiz),
+        splashRadius: 20,
+      ),
+
+    ],
+  );
+}
+
+
